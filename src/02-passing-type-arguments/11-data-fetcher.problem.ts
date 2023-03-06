ecbc8370ee6ff817decc6f728ff00fd59efb255c
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-03-06 13:26:28 +0100

    complete sections 1 and 2

--- src/02-passing-type-arguments/11-data-fetcher.problem.ts
+++ src/02-passing-type-arguments/11-data-fetcher.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fetchData = async (url: string) => {
+const fetchData = async <TData>(url: string): Promise<TData> => {
   const data = await fetch(url).then((response) => response.json());
   return data;
 };
